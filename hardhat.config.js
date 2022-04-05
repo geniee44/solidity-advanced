b8561d83dfd7394ccf2864ec970e20e81f11df85
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-04-06 00:12:18 +0900

    verify success

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@
 
 require("@nomiclabs/hardhat-waffle");
 require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
 
 const { mnemonic } = require('./secrets.json');
 
@@ -17,9 +18,12 @@ const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
+// const ALCHEMY_API_KEY = "o6Tu3uXD9IOX1-LRD4ulFLRcYFVZZ6pl"
+// const ROPSTEN_PRIVATE_KEY = "15031c173478a30b7fccaf60088d36772fa8454f02aa84a84a4a8b7028e2c86e"
+
 module.exports = {
   solidity: {
-    version: "0.8.7"
+    version: "0.8.0"
   },
   networks: {
     ropsten: {
